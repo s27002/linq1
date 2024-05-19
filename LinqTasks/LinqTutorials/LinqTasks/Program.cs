4e1980010227a9ab8cb2454f82184c8455b76c68
unknown <s27002@pjwstk.edu.pl>
2024-05-19 22:51:17 +0200

    done

---
+++ LinqTasks/LinqTutorials/LinqTasks/Program.cs
@@ -0,0 +1,18 @@
+﻿using LinqTasks.Models;
+
+namespace LinqTasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("======= ZAD X =======");
+
+        IEnumerable<Object> result = Tasks.Task11();
+
+        foreach (Object emp in result)
+        {
+            Console.WriteLine(emp);
+        }
+    }
+}
